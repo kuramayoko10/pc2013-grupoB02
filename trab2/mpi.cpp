eb40b0b202ffdbf8145cbd56ffe54e8232d88bce
Rodrigo Beber <rodrigo@rodrigo.(none)>
2013-04-25 17:02:21 -0300

    Alterando alguams coisas, ainda nao esta funfando

--- trab2/mpi.cpp
+++ trab2/mpi.cpp
@@ -4,17 +4,17 @@
 #include <math.h>
 
 #define NUMBER_OF_NODES 13
-#define SIZE_OF_SEGMENT ceil(54261766/NUMBER_OF_NODES)
+#define SIZE_OF_SEGMENT (int)ceil(54261766/NUMBER_OF_NODES)
 
 
 int main(int argc, char **argv)
 {
 	int numtasks, rank, rc, dest, source, tag=1;
 	char *outmsg[NUMBER_OF_NODES]; 
-	char *inmsg;
+	char *inmsg[NUMBER_OF_NODES];
 	char fileName[30], aux;
 	register unsigned long int count=0, count2=0, incrementOfSize=1;
-	unsigned long int sizeOfVector[NUMBER_OF_NODES], maior = SIZE_OF_SEGMENT;
+	unsigned long int sizeOfVector[NUMBER_OF_NODES];
 	FILE *file;
 	MPI_Status Stat;
 
@@ -37,14 +37,13 @@ int main(int argc, char **argv)
 	     	printf("Arquivo de entrada nao encontrado!\n");
 	        exit(1);
 	    }
-
 		/*inicializa os vetores de caracteres para enviar a msg*/
 		for(count=0; count < NUMBER_OF_NODES; count++)
 			outmsg[count] = (char*) malloc (SIZE_OF_SEGMENT*sizeof(char ));
 			
 		for(count=0; count < NUMBER_OF_NODES; count++)
 			outmsg[count][0] = '\0';
-
+		
 		/*para cada vetor, completa com trechos do texto, chegando ate o seu máximo, depois disso, continua a inserir dados no vetor até que seja 			encontrado um final de frase(no caso ponto, ponto de exclamação, interrogação ou ENTER)*/
 		for(count2=0; count2 < NUMBER_OF_NODES; count2++){
 			for(count=0;!feof(file) && count < SIZE_OF_SEGMENT; count++){
@@ -64,24 +63,24 @@ int main(int argc, char **argv)
 			incrementOfSize = 1;
 			outmsg[count2][count] = '\0';
 			sizeOfVector[count2] = count; 
-			if(count > maior)
-				maior = count;
 		}
-		
-		for(dest = 1; dest <= NUMBER_OF_NODES; dest++)
+		for(dest = 1; dest <= NUMBER_OF_NODES; dest++){
 			rc = MPI_Send(&outmsg[dest-1], sizeOfVector[dest-1], MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+		}
 	}
 	else{
 		source = 0;	
 		printf("rank:%d\n", rank);	
+		int tamanho;
+		MPI_Get_count(&Stat,MPI_CHAR,&tamanho);
 
 		/*assim eu garanto que toda msg que eu receber poderá ser armazenada no vetor inmsg*/
-		inmsg = (char*) malloc (maior*sizeof(char));
+		inmsg[rank-1] = (char*) malloc (tamanho*sizeof(char));
+//		printf("%d\n", tamanho);
+				
+		rc = MPI_Recv(&inmsg[rank-1], tamanho, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
 	
-		rc = MPI_Recv(&inmsg, sizeOfVector[rank-1], MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
 		
-		
-
 
 		/*aqui chama a funcao sequencial para calcular se eh palindrome ou nao*/
 	}
