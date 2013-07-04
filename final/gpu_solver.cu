9bcfce6b3e9e47b6d09fb0841805e1ceafc39972
Rodrigo Beber <rodrigo@rodrigo.(none)>
2013-07-04 13:25:38 -0300

    atualizando

--- final/gpu_solver.cu
+++ final/gpu_solver.cu
@@ -3,6 +3,8 @@
 #include "extern.h"
 #include "smatrix.h"
 
+#define check(X) assert(X==cudaSuccess)
+
 __global__ void kernel(float *a, float *b, float *c, unsigned order)
 {
 	int i = threadIdx.x + blockIdx.x;
@@ -14,22 +16,26 @@ __global__ void kernel(float *a, float *b, float *c, unsigned order)
 
 float *solve(float *A, float *b)
 {
-	unsigned i;
+	unsigned i, it=0;
 	float *x;	
-	float *gpu_x, *gpu_b, *gpu_c;
+	float *gpu_A, *gpu_x, *gpu_b, *gpu_c;
 	x = vector_new(order);
 	for (i=0; i<order; ++i)
-		x[i] = 1;
-	cudaMalloc((void **)&gpu_x, order * sizeof(float));
-	cudaMalloc((void **)&gpu_b, order * sizeof(float));
-	cudaMalloc((void **)&gpu_c, order * sizeof(float));
-	assert(cudaMemcpy(gpu_x, x, order * sizeof(float), cudaMemcpyHostToDevice)==cudaSuccess);
-	assert(cudaMemcpy(gpu_b, b, order * sizeof(float), cudaMemcpyHostToDevice)==cudaSuccess);
-	kernel<<<order, 1>>>(gpu_x, gpu_b, gpu_c, order);	
-	assert(cudaMemcpy(x, gpu_c, order * sizeof(float), cudaMemcpyDeviceToHost)==cudaSuccess);
-	vector_print(x);
-	cudaFree(gpu_c);
+		x[i] = 1.0;
+	check(cudaMalloc((void **)&gpu_A, order * order *sizeof(float)));
+	check(cudaMalloc((void **)&gpu_b, order * sizeof(float)));
+	check(cudaMemcpy(gpu_A, A, order * order * sizeof(float), cudaMemcpyHostToDevice));
+	check(cudaMemcpy(gpu_b, b, order * sizeof(float), cudaMemcpyHostToDevice));
+	while (it<it_num)
+	{
+		cudaMemcpy(gpu_x, x, order * sizeof(float), cudaMemcpyHostToDevice);
+		kernel<<<order, 1>>>(gpu_x, gpu_b, gpu_c, order);	
+		cudaMemcpy(x, gpu_x, order * sizeof(float), cudaMemcpyDeviceToHost);
+		++it;
+	}
+	cudaFree(gpu_A);
 	cudaFree(gpu_b);
-	cudaFree(gpu_x);
+	vector_print(x);
 	return x;
 }
+
